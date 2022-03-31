06728da8d79ccd12b7e9fafcc5fe5ba525b670b0
Ratul Hasan <ratulhasanshaon@gmail.com>
2022-03-31 14:45:40 +0600

    STATICFILES_STORAGE

--- backend/settings.py
+++ backend/settings.py
@@ -94,7 +94,6 @@ MIDDLEWARE = [
 
 
     'django.middleware.security.SecurityMiddleware',
-
     "whitenoise.middleware.WhiteNoiseMiddleware",
 
 
@@ -175,8 +174,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
